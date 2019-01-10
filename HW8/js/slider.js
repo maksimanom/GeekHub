1807a4905c5eed8f1f82898fe74a98b5f294f40f
maksimanom <maksymKozoriz@gmail.com>
2019-01-10 22:38:11 +0200

    changed buttons to addEventListener

--- HW8/js/slider.js
+++ HW8/js/slider.js
@@ -1,23 +1,49 @@
+let arrowLeft = document.getElementById('arrowLeft'),
+    arrowRight = document.getElementById('arrowRight');
 let slideIndex = 2;
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-   let i;
-   let x = document.getElementsByClassName("selected-date");
-   let z = document.getElementsByClassName('date-of-performance');
-   if (n > x.length) {slideIndex = 1}
-   if (n < 1) {slideIndex = x.length} ;
-   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-   }
+arrowLeft.addEventListener('click', function () {
+    let i;
+    let x = document.getElementsByClassName("selected-date");
+    let z = document.getElementsByClassName('date-of-performance');
+    if (slideIndex > x.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < x.length; i++) {
+      z[i].classList.remove('date-of-performance-default');
+      z[i].classList.remove('date-of-performance-selected');
+      z[i].classList.add('date-of-performance');
+    }
+    x[slideIndex-1].style.display = "flex";
+    z[slideIndex-1].classList.toggle('date-of-performance-selected');
+    slideIndex -= 1;
+});
+arrowRight.addEventListener('click', function () {
+  let i;
+  let x = document.getElementsByClassName("selected-date");
+  let z = document.getElementsByClassName('date-of-performance');
+  if (slideIndex >= x.length) {slideIndex = 0}
+  if (slideIndex < 1) {this.slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
   for (i = 0; i < x.length; i++) {
     z[i].classList.remove('date-of-performance-default');
     z[i].classList.remove('date-of-performance-selected');
     z[i].classList.add('date-of-performance');
   }
-   x[slideIndex-1].style.display = "flex";
-   z[slideIndex-1].classList.toggle('date-of-performance-selected')
+  x[slideIndex].style.display = "flex";
+  z[slideIndex].classList.toggle('date-of-performance-selected');
+  slideIndex += 1;
+});
+
+function showSelectedPerformance(n) {
+  let x = document.getElementsByClassName("selected-date");
+  let z = document.getElementsByClassName('date-of-performance');
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
 }
